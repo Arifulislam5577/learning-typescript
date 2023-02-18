7f4602274a84fd561ee4bae3b401622fe5158499
Arif5577 <arifalamm33@gmail.com>
2023-02-18 15:44:58 +0600

    learn typescript

--- setup/dist/index.js
+++ setup/dist/index.js
@@ -5,7 +5,15 @@ class User {
         this.name = name;
         this.id = `${new Date().toISOString()}`;
     }
+    get getId() {
+        return this.id;
+    }
+    set newId(newId) {
+        this.id = newId;
+    }
 }
 const user1 = new User("a@gmail.com", "Arif");
 user1.name = "Arif Khan";
+user1.newId = "123";
+console.log(user1.getId);
 console.log(user1);
--- setup/src/index.ts
+++ setup/src/index.ts
@@ -1,8 +1,18 @@
 class User {
-  private readonly id: string = `${new Date().toISOString()}`;
+  private id: string = `${new Date().toISOString()}`;
   constructor(readonly email: string, public name: string) {}
+
+  get getId(): string {
+    return this.id;
+  }
+
+  set newId(newId: string) {
+    this.id = newId;
+  }
 }
 
 const user1 = new User("a@gmail.com", "Arif");
 user1.name = "Arif Khan";
+user1.newId = "123";
+console.log(user1.getId);
 console.log(user1);
